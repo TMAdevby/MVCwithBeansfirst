46a703da9441f909dbaa68b16f57ae95d1b23d30
TMAdevby <mtipun@mail.ru>
2026-08-29 15:27:59 +0300

    first

---
+++ src/main/java/com/example/mvcwithbeansfirst/MvCwithBeansfirstApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvcwithbeansfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvCwithBeansfirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvCwithBeansfirstApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mvcwithbeansfirst/MvCwithBeansfirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvcwithbeansfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvCwithBeansfirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
