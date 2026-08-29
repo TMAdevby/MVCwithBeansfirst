e4054bb9d3e31fb149f9a50e83aa02013f3df028
TMAdevby <mtipun@mail.ru>
2026-08-29 15:47:05 +0300

    commit 2

--- src/main/java/com/example/mvcwithbeansfirst/MvcWithBeansFirstApplication.java
+++ src/main/java/com/example/mvcwithbeansfirst/MvcWithBeansFirstApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MvCwithBeansfirstApplication {
-
+public class MvcWithBeansFirstApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MvCwithBeansfirstApplication.class, args);
+        SpringApplication.run(MvcWithBeansFirstApplication.class, args);
     }
-
 }
